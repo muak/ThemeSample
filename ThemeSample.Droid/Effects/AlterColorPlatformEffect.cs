45f9b1d630867ffff46276f9b6ec1b3854b8086d
kamu <kamuxxx@hotmail.com>
2016-12-13 17:18:18 +0900

    多分完成〜

--- ThemeSample.Droid/Effects/AlterColorPlatformEffect.cs
+++ ThemeSample.Droid/Effects/AlterColorPlatformEffect.cs
@@ -3,14 +3,18 @@ using Xamarin.Forms.Platform.Android;
 using Xamarin.Forms;
 using ThemeSample.Effects;
 using ThemeSample.Droid.Effects;
+using Android.Widget;
+using Android.Graphics.Drawables;
+using Android.Support.V7.Widget;
+using Android.Content.Res;
 
 [assembly: ResolutionGroupName("Xamarin")]
 [assembly: ExportEffect(typeof(AlterColorPlatformEffect), nameof(AlterColor))]
 namespace ThemeSample.Droid.Effects
 {
-    public class AlterColorPlatformEffect:PlatformEffect
+    public class AlterColorPlatformEffect : PlatformEffect
     {
-       
+
         protected override void OnAttached()
         {
             UpdateColor();
@@ -18,7 +22,7 @@ namespace ThemeSample.Droid.Effects
 
         protected override void OnDetached()
         {
-            
+
         }
 
         void UpdateColor()
@@ -27,12 +31,63 @@ namespace ThemeSample.Droid.Effects
             if (Element is NavigationPage) {
                 UpdateStatusBar(color);
             }
+            else if (Element is Slider) {
+                UpdateSlider(color);
+            }
+            else if (Element is Xamarin.Forms.Switch) {
+                UpdateSwitch(color);
+            }
         }
 
         void UpdateStatusBar(Android.Graphics.Color color)
         {
             var window = (Container.Context as FormsAppCompatActivity).Window;
             window.SetStatusBarColor(color);
+        }
+
+        void UpdateSlider(Android.Graphics.Color color)
+        {
+            var seekBar = Control as SeekBar;
+
+            var progress = (LayerDrawable)(seekBar.ProgressDrawable.Current);
+
+            progress.GetDrawable(2).SetTint(color);
+            var altColor = Android.Graphics.Color.Argb(76, color.R, color.G, color.B);
+            progress.GetDrawable(0).SetTint(altColor);
+
+            seekBar.Thumb.SetTint(color);
+        }
+
+        void UpdateSwitch(Android.Graphics.Color color)
+        {
+
+            var aSwitch = Control as SwitchCompat;
+
+            var trackColors = new ColorStateList(new int[][]
+                 {
+                                new int[]{global::Android.Resource.Attribute.StateChecked},
+                                new int[]{-global::Android.Resource.Attribute.StateChecked},
+                 },
+                new int[] {
+                                Android.Graphics.Color.Argb(76,color.R,color.G,color.B),
+                                Android.Graphics.Color.Argb(76, 117, 117, 117)
+                 });
+
+
+            aSwitch.TrackDrawable.SetTintList(trackColors);
+
+            var thumbColors = new ColorStateList(new int[][]
+                 {
+                                new int[]{global::Android.Resource.Attribute.StateChecked},
+                                new int[]{-global::Android.Resource.Attribute.StateChecked},
+                 },
+                new int[] {
+                                color,
+                                Android.Graphics.Color.Argb(255, 244, 244, 244)
+                 });
+
+            aSwitch.ThumbDrawable.SetTintList(thumbColors);
+
         }
 
         protected override void OnElementPropertyChanged(System.ComponentModel.PropertyChangedEventArgs args)
---
+++ ThemeSample.iOS/Effects/AlterColorPlatformEffect.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using ThemeSample.Effects;
+using ThemeSample.iOS.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using UIKit;
+
+[assembly: ResolutionGroupName("Xamarin")]
+[assembly: ExportEffect(typeof(AlterColorPlatformEffect), nameof(AlterColor))]
+namespace ThemeSample.iOS.Effects
+{
+    public class AlterColorPlatformEffect : PlatformEffect
+    {
+
+
+        protected override void OnAttached()
+        {
+            UpdateColor();
+        }
+
+        protected override void OnDetached()
+        {
+
+        }
+
+        void UpdateColor()
+        {
+            var color = AlterColor.GetColor(Element).ToUIColor();
+            if (Element is Slider) {
+                UpdateSlider(color);
+            }
+            else if (Element is Switch) {
+                UpdateSwitch(color);
+            }
+        }
+
+        void UpdateSlider(UIColor color)
+        {
+            var slider = Control as UISlider;
+            slider.MinimumTrackTintColor = color;
+            slider.MaximumTrackTintColor = color.ColorWithAlpha(0.3f);
+        }
+
+        void UpdateSwitch(UIColor color)
+        {
+            var uiSwitch = Control as UISwitch;
+            uiSwitch.OnTintColor = color;
+            //Offの時の背景を変えるならこれ
+            //uiSwitch.BackgroundColor = UIColor.FromRGB(244,244,244);
+            //uiSwitch.Layer.CornerRadius = uiSwitch.Frame.Size.Height / 2;
+        }
+
+        protected override void OnElementPropertyChanged(System.ComponentModel.PropertyChangedEventArgs args)
+        {
+            base.OnElementPropertyChanged(args);
+
+            if (args.PropertyName == AlterColor.ColorProperty.PropertyName) {
+                UpdateColor();
+            }
+        }
+    }
+}
